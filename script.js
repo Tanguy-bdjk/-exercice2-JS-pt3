f395fa9f2502c4672a96fe57462adc794adc2024
Tanguy-bdjk <tanguybondjokwe@gmail.com>
2018-10-14 14:18:01 +0200

    js

---
+++ script.js
@@ -0,0 +1,8 @@
+function afficher() {// fonction en anglais
+  document.getElementById('text').style.display='block';
+}
+
+function masquer() {
+  document.getElementById('text').style.display='none';
+
+}
